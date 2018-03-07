8b72eee2ef3d75a065b7df7944f3124134f71973
Shen Xiaoli <xiaoli.shen@fastretailing.com>
2018-03-07 17:34:22 +0900

    middle stage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import createHistory from 'history/createBrowserHistory'
+import { Route } from 'react-router'
+
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+
+import reducers from './reducers' // Or wherever you keep your reducers
+
+import Home from './containers/Home';
+import About from './containers/About';
+import Topics from './containers/Topics';
+
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history)
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+)
+
+// Now you can dispatch navigation actions from anywhere!
+// store.dispatch(push('/foo'))
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  render(){
+    return <Provider store={store}>
+        { /* ConnectedRouter will use the store from Provider automatically */ }
+        <ConnectedRouter history={history}>
+          <div>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
+          </div>
+        </ConnectedRouter>
+      </Provider>  
   }
 }
 
---
+++ src/containers/Topics.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Topics extends React.Component {
+  render(){
+    return <h1>Topics</h1>;
+  }
+}
+
+export default Topics;
