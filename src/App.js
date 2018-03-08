bfc5f86e70effc489ce10d810c5ffda2ea3a6a1d
furixturi <alabebop@gmail.com>
2018-03-08 20:55:05 +0900

    restructure index.js and App.js to prepare for saga integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from "react";
 
-import { connect } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { connect, Provider } from "react-redux";
+
+import logger from "redux-logger";
+
 import { Route, Switch } from "react-router";
-import { ConnectedRouter } from "react-router-redux";
+import { ConnectedRouter, routerMiddleware } from "react-router-redux";
 
 import history from './helpers/history';
+
+import rootReducer from "./reducers/rootReducer";
+
 import Home from "./containers/Home";
 import About from "./containers/About";
 import Topics from "./containers/Topics";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(routerMiddleware(history), logger)
+);
 
 const ConnectedSwitch = connect(state => ({
     location: state.router.location
 }))(Switch);
 
 const App = () => (
+  <Provider store={store}>
     <ConnectedRouter history={history}>
-        <ConnectedSwitch>
-            <Route exact path='/' component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/topics' component={Topics} />
-        </ConnectedSwitch>
+      <ConnectedSwitch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </ConnectedSwitch>
     </ConnectedRouter>
+  </Provider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux"
-import logger from "redux-logger";
-
-import { routerMiddleware } from "react-router-redux";
-
-import rootReducer from './reducers/rootReducer';
-import history from './helpers/history';
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(routerMiddleware(history), logger)
-);
-
-render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
+render(<App />,
   document.getElementById("root")
 );
 
