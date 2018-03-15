a60a8b0209d6f0e6334e1e842f968604e05d019e
Xiaoli Shen <xiaoli.shen@fastretailing.com>
2018-03-15 10:17:04 +0900

    hot module reload for sass modules

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -191,21 +191,19 @@ module.exports = {
           },
           {
             test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: [
-                {
-                  loader: 'css-loader',
-                  options: {
-                    modules: true,
-                    sourceMap: true,
-                    importLoaders: 2,
-                    localIdentName: '[name]__[local]__[hash:base64:5]'
-                  }
-                },
-                'sass-loader'
-              ]
-            })
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  modules: true,
+                  sourceMap: true,
+                  importLoaders: 2,
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
+                }
+              },
+              require.resolve('sass-loader')
+            ]
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
